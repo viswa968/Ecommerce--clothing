6e2a15d10de688e1a5e8ad08bcfa19a28ebe1438
Hi <viswa968@msitprogram.net>
2020-11-04 18:17:33 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component';
+
+
+function App() {
+  return ( 
+    <HomePage />
+  );
+}
+
+export default App;
